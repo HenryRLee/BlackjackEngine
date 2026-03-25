edd2a0fa74b0d5ffb3a19c42fdaf1df07badb0cb
HenryLee <lee0906@hotmail.com>
2026-03-26 10:22:04 +1100

    Add more tests to HandScore adding a CardScore

--- cpp/include/hand.h
+++ cpp/include/hand.h
@@ -8,23 +8,22 @@ struct HandScore {
   bool isSoft = false;
 
   explicit HandScore(unsigned v) : score(v) { }
+  HandScore(unsigned v, bool soft) : score(v), isSoft(soft) { }
 
   HandScore& operator+=(CardScore card) {
 	score = score + card.score;
 
-	if (score > 21) {
-		if (card.score == 11) {
-			score -= 10;
-		}
-		else if (isSoft)
-		{
-			score -= 10;
-			isSoft = false;
-		}
-	}
-	else if (card.score == 11) {
-		isSoft = true;
-	}
+    if (score > 21) {
+      if (card.score == 11) {
+        score -= 10;
+      }
+      else if (isSoft) {
+        score -= 10;
+        isSoft = false;
+      }
+    } else if (card.score == 11) {
+      isSoft = true;
+    }
 
 	return *this;
   }
--- cpp/tests/hand.cpp
+++ cpp/tests/hand.cpp
@@ -1,10 +1,131 @@
 #include <gtest/gtest.h>
 #include "hand.h"
 
-TEST(HandScoreTests, TestAddCard) {
-  HandScore hand(12);
+TEST(HandScoreTests, TestBasicAddCard) {
+  {
+    HandScore hand(2);
+    hand += CardScore(3);
+    EXPECT_EQ(hand.score, 5);
+    EXPECT_EQ(hand.isSoft, false);
+  }
 
-  hand += CardScore(3);
+  {
+    HandScore hand(12);
+    hand += CardScore(3);
+    EXPECT_EQ(hand.score, 15);
+    EXPECT_EQ(hand.isSoft, false);
+  }
 
-  EXPECT_EQ(hand.score, 15);
+  {
+    HandScore hand(10);
+    hand += CardScore(3);
+    EXPECT_EQ(hand.score, 13);
+    EXPECT_EQ(hand.isSoft, false);
+  }
+}
+
+TEST(HandScoreTests, TestAddCardThatBursts) {
+  {
+    HandScore hand(19);
+    hand += CardScore(3);
+    EXPECT_EQ(hand.score, 22);
+    EXPECT_EQ(hand.isSoft, false);
+  }
+
+  {
+    HandScore hand(21);
+    hand += CardScore(11);
+    EXPECT_EQ(hand.score, 22);
+    EXPECT_EQ(hand.isSoft, false);
+  }
+
+  {
+    HandScore hand(17);
+    hand += CardScore(5);
+    EXPECT_EQ(hand.score, 22);
+    EXPECT_EQ(hand.isSoft, false);
+  }
+}
+
+TEST(HandScoreTests, TestAddAce) {
+  {
+    HandScore hand(2);
+    hand += CardScore(11);
+    EXPECT_EQ(hand.score, 13);
+    EXPECT_EQ(hand.isSoft, true);
+  }
+
+  {
+    HandScore hand(9);
+    hand += CardScore(11);
+    EXPECT_EQ(hand.score, 20);
+    EXPECT_EQ(hand.isSoft, true);
+  }
+
+  {
+    HandScore hand(13);
+    hand += CardScore(11);
+    EXPECT_EQ(hand.score, 14);
+    EXPECT_EQ(hand.isSoft, false);
+  }
+
+  {
+    HandScore hand(20);
+    hand += CardScore(11);
+    EXPECT_EQ(hand.score, 21);
+    EXPECT_EQ(hand.isSoft, false);
+  }
+}
+
+TEST(HandScoreTests, TestSoftHandAddCard) {
+  {
+    HandScore hand(11, true);
+    hand += CardScore(11);
+    EXPECT_EQ(hand.score, 12);
+    EXPECT_EQ(hand.isSoft, true);
+  }
+
+  {
+    HandScore hand(13, true);
+    hand += CardScore(7);
+    EXPECT_EQ(hand.score, 20);
+    EXPECT_EQ(hand.isSoft, true);
+  }
+
+  {
+    HandScore hand(12, true);
+    hand += CardScore(9);
+    EXPECT_EQ(hand.score, 21);
+    EXPECT_EQ(hand.isSoft, true);
+  }
+
+  {
+    HandScore hand(20, true);
+    hand += CardScore(9);
+    EXPECT_EQ(hand.score, 19);
+    EXPECT_EQ(hand.isSoft, false);
+  }
+
+  {
+    HandScore hand(17, true);
+    hand += CardScore(5);
+    EXPECT_EQ(hand.score, 12);
+    EXPECT_EQ(hand.isSoft, false);
+  }
+
+  {
+    HandScore hand(13, true);
+    hand += CardScore(11);
+    EXPECT_EQ(hand.score, 14);
+    EXPECT_EQ(hand.isSoft, true);
+  }
+
+
+  {
+    HandScore hand(20, true);
+    hand += CardScore(11);
+    EXPECT_EQ(hand.score, 21);
+    EXPECT_EQ(hand.isSoft, true);
+  }
+
 }
